5c61aa1f20a85c29112bbf382fb7e4f754a46d1e
abusaleh2234 <you@example.com>
2023-11-10 01:18:55 +0600

    job update complete

--- index.js
+++ index.js
@@ -32,24 +32,61 @@ async function run() {
     const jobsCollection = client.db("jobDraftDB").collection("jobs");
 
 
-    app.get("/jobs" , async (req, res) => {
-        const result = await jobsCollection.find().toArray()
-        res.send(result)
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray()
+      res.send(result)
     })
     app.get("/jobs/:ctgname", async (req, res) => {
-        const category = req.params.ctgname
-        console.log(category);
-        const filter = {category: category}
-        const result = await jobsCollection.find(filter).toArray()
-        res.send(result)
+      const category = req.params.ctgname
+      // console.log(category);
+      const filter = { category: category }
+      const result = await jobsCollection.find(filter).toArray()
+      res.send(result)
     })
     app.get("/jobsdetails/:id", async (req, res) => {
-        const id = req.params.id
-        console.log(id);
-        const filter = {_id: new ObjectId(id)}
-        console.log(filter);
-        const result = await jobsCollection.findOne(filter)
-        res.send(result)
+      const id = req.params.id
+      // console.log(id);
+      const filter = { _id: new ObjectId(id) }
+      // console.log(filter);
+      const result = await jobsCollection.findOne(filter)
+      res.send(result)
+    })
+
+    app.post("/jobspost", async (req, res) => {
+      const newJob = req.body
+      // console.log(newJob);
+      const result = await jobsCollection.insertOne(newJob)
+      res.send(result)
+    })
+
+    app.put("/updatejob/:id", async (req, res) => {
+      const id = req.params.id
+      console.log(id);
+      const job = req.body
+      console.log(job);
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updateJob = {
+        $set: {
+          post_creator_name: job.post_creator_name,
+          job_title: job.job_title,
+          category: job.category,
+          job_posting_date: job.job_posting_date,
+          application_deadline: job.application_deadline,
+          salary_range: job.salary_range,
+          job_applicants_number: job.job_applicants_number,
+          job_banner_img: job.job_banner_img,
+          job_description: job.job_description,
+          creator_email: job.creator_email,
+        },
+      };
+      const result = await jobsCollection.updateOne(filter, updateJob, options)
+      res.send(result)
+    })
+
+    app.delete("/myjobdelete/:id", async (req, res) => {
+      const id = req.params.id
+      console.log(id);
     })
 
 
@@ -67,9 +104,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('jobdraft has boon runung soon')
+  res.send('jobdraft has boon runung soon')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
